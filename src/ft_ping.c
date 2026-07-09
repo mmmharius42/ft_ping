4a254b2cf826fa219de2228acf917507ac67a3d0
mmmharius <mariuspapin@rcmm.fr>
2026-07-09 11:12:28 +0200

    starting

---
+++ src/ft_ping.c
@@ -0,0 +1,14 @@
+#include <ft_ping.h>
+
+
+void    parse_arg(int nb, char **arg) {
+    if (nb > 2)
+}
+
+
+int main(int ac, char **av) {
+    (void)av;
+    if (ac == 1)
+        printf("ping: usage error: Destination address required\n");
+    parse_arg(ac, av);
+}
